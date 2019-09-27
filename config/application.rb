2349be917d5c83149d4acb69d8d035bd246a7a51
ramlohith <ram.lohith@outlook.com>
2019-09-27 13:36:21 -0400

    Changes made today

--- config/application.rb
+++ config/application.rb
@@ -9,11 +9,14 @@ Bundler.require(*Rails.groups)
 module DigitalLibrary
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+   # config.load_defaults 6.0
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    version = Rails.version.to_f
+    config.active_record.sqlite3.represent_boolean_as_integer = true
+    config.secret_token = Digest::SHA1.hexdigest Time.now.to_s if version < 5.2
   end
 end
