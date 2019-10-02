00d88dc5aab8803c5c46c0c5020cd224fe968341
ramlohith <ram.lohith@outlook.com>
2019-10-02 13:33:38 -0400

    Merge branch 'master' of https://github.com/GarimaGarima/CSC517-DigitalLibrary into GarimaGarima-master
    # Conflicts:
    #	.idea/DigitalLibrary.iml
    #	app/controllers/librarian_controller.rb
    #	app/controllers/sessions_controller.rb
    #	app/views/students/index.html.erb
    #	config/routes.rb
    #	db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,28 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_29_044954) do
-
-  create_table "active_storage_attachments", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.integer "record_id", null: false
-    t.integer "blob_id", null: false
-    t.datetime "created_at", null: false
-    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
-    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
-  end
-
-  create_table "active_storage_blobs", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.bigint "byte_size", null: false
-    t.string "checksum", null: false
-    t.datetime "created_at", null: false
-    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
-  end
+ActiveRecord::Schema.define(version: 2019_10_02_173200) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email"
@@ -53,28 +32,49 @@ ActiveRecord::Schema.define(version: 2019_09_29_044954) do
   end
 
   create_table "book_requests", force: :cascade do |t|
-    t.integer "library_id"
+    t.string "library_name"
     t.string "isbn"
     t.integer "books_available"
     t.integer "books_checkedout"
     t.integer "books_holdrequest"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", precision: 6
+    t.datetime "updated_at", precision: 6
   end
 
   create_table "bookmarks", force: :cascade do |t|
     t.string "student_name"
     t.string "student_email"
     t.string "isbn"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.string "title"
     t.string "author"
     t.integer "edition"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
-# Could not dump table "books" because of following StandardError
-#   Unknown type 'avatar' for column 'image'
+  create_table "books", force: :cascade do |t|
+    t.string "isbn"
+    t.string "title"
+    t.string "author"
+    t.string "language"
+    t.string "published"
+    t.integer "edition"
+    t.binary "image"
+    t.string "subject"
+    t.text "summary"
+    t.boolean "special"
+    t.datetime "created_at", precision: 6
+    t.datetime "updated_at", precision: 6
+    t.string "library"
+    t.integer "number_available"
+    t.integer "number_checkedout"
+    t.integer "number_holdrequest"
+    t.string "university"
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
+  end
 
   create_table "history_requests", force: :cascade do |t|
     t.string "library_name"
@@ -84,7 +84,7 @@ ActiveRecord::Schema.define(version: 2019_09_29_044954) do
     t.string "student_email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.decimal "fines"
+    t.decimal "fines", default: "0.0"
   end
 
   create_table "hold_requests", force: :cascade do |t|
@@ -114,8 +114,8 @@ ActiveRecord::Schema.define(version: 2019_09_29_044954) do
     t.string "location"
     t.integer "maxdays"
     t.decimal "fine"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", precision: 6
+    t.datetime "updated_at", precision: 6
   end
 
   create_table "students", force: :cascade do |t|
@@ -129,5 +129,4 @@ ActiveRecord::Schema.define(version: 2019_09_29_044954) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
 end
