ddfc0df8cffb2484b59bbe3231b0464609c4ea05
ramlohith <ram.lohith@outlook.com>
2019-10-02 13:45:49 -0400

    Changes for prod-->pg

--- db/migrate/20191002173200_change_edition_to_int.rb
+++ db/migrate/20191002173200_change_edition_to_int.rb
@@ -1,5 +1,5 @@
 class ChangeEditionToInt < ActiveRecord::Migration[6.0]
   def change
-    change_column :books, :edition, 'integer USING CAST(column_name AS integer)'
+    change_column :books, :edition, 'integer USING CAST(edition AS integer)'
   end
 end
