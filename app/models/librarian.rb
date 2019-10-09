c288fd24df7a7af5772702baa4d56b66b87d3a0b
garimas <garimasarangal@gmail.com>
2019-10-09 11:27:10 -0500

    Approval by admin for librarian, books edit change by librarian and admin

--- app/models/librarian.rb
+++ app/models/librarian.rb
@@ -1,7 +1,7 @@
 class Librarian < ApplicationRecord
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } , uniqueness: true, presence: true
   validates :name, presence: true
-  validates :password, presence: true
+  validates :password, presence: true, length: { minimum: 6 }, on: :create
   validates :library, presence: true
   has_secure_password validations: false
 end
